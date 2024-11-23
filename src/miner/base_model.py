1de890ef711137fbce33e33f7c0e28990060b74d
mltrev23 <trevor.dev23@gmail.com>
2024-11-23 12:08:42 -0600

    feat: preprocess

--- src/miner/base_model.py
+++ src/miner/base_model.py
@@ -1,6 +1,11 @@
+import numpy as np
 import pandas as pd
 from pandas import DataFrame
-import ta
+
+from ta.trend import MACD
+from ta.momentum import RSIIndicator, ROCIndicator
+
+from sklearn.preprocessing import MinMaxScaler
 
 from db.db_manager import DBManager
 
@@ -15,20 +20,31 @@ def load_datasets_from_db():
     
     input['SMA_50'] = input['price'].rolling(window=50).mean()
     input['SMA_200'] = input['price'].rolling(window=200).mean()
-    input['RSI'] = ta.momentum.RSIIndicator(input['price']).rsi()
-    input['Momentum'] = ta.momentum.ROCIndicator(input['price']).roc()
-    input['MACD'] = ta.trend.macd(input['price'])
+    input['RSI'] = RSIIndicator(input['price']).rsi()
+    input['Momentum'] = ROCIndicator(input['price']).roc()
+    input['MACD'] = MACD(input['price']).macd()
     
     for i in range(1, 1 + PREDICTION_COUNT):
         input[f'NextPrice{i}'] = input['price'].shift(-1 * i)
-    print(input.iloc[:547])
-        
+
+    input.replace([np.inf, -np.inf], np.nan, inplace = True)        
     input.dropna(inplace = True)
     print(input)
+    
+    return input
 
-def create_base_lstm():
-    pass
+def preprocess(dataset: DataFrame):
+    X = dataset[['price', 'SMA_50', 'SMA_200', 'RSI', 'Momentum', 'MACD']].values
+    y = dataset[['NextPrice1', 'NextPrice2', 'NextPrice3', 'NextPrice4', 'NextPrice5', 'NextPrice6']].values
+    print(X)
+    
+    X_scaler = MinMaxScaler(feature_range=(0, 1))
+    y_scaler = MinMaxScaler(feature_range=(0, 1))
+    X_scaled = X_scaler.fit_transform(X)
+    y_scaled = y_scaler.fit_transform(y)
+    
+    return X_scaler, y_scaler, X_scaled, y_scaled
 
 if __name__ == '__main__':
-    load_datasets_from_db()
-    pass
+    dataset = load_datasets_from_db()
+    preprocess(dataset)
