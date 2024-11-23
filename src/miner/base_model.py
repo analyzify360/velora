76819bceef5dcb0a9bb29d86681dff228296d7dc
mltrev23 <trevor.dev23@gmail.com>
2024-11-23 10:12:58 -0600

    feat: load dataset from db & save as dataframe

--- src/miner/base_model.py
+++ src/miner/base_model.py
@@ -1,10 +1,16 @@
+import pandas as pd
+from db.db_manager import DBManager
 
+db_manager = DBManager()
 
 def load_datasets_from_db():
-    pass
+    pool_address = '0x04916039b1f59d9745bf6e0a21f191d1e0a84287'
+    result = pd.read_sql(f"select * from uniswap_signals where pool_address='{pool_address}'", db_manager.engine)
+    print(result)
 
 def create_base_lstm():
     pass
 
 if __name__ == '__main__':
+    load_datasets_from_db()
     pass
