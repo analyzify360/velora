fd7408c5c6c77b9f1fe220cc77411c58c95db66f
drunest <venus1004@proton.me>
2024-12-04 12:45:43 -0600

    feat: add new forward method

--- src/miner/miner.py
+++ src/miner/miner.py
@@ -1,4 +1,4 @@
-from communex.module import Module, endpoint
+from communex.module.module import Module, endpoint
 from communex.key import generate_keypair
 from communex.compat.key import classic_load_key
 from keylimiter import TokenBucketLimiter
@@ -11,7 +11,8 @@ from uniswap_fetcher_rs import UniswapFetcher
 from utils.protocols import (HealthCheckSynapse, HealthCheckResponse,
                              PoolEventSynapse, PoolEventResponse,
                              PoolMetricSynapse, PoolMetricResponse,
-                             PredictionSynapse, PredictionSynapse)
+                             PredictionSynapse, PredictionSynapse,
+                             CurrentPoolMetricSynapse, CurrentPoolMetricResponse)
 from db.db_manager import DBManager
 
 class Miner(Module):
@@ -64,6 +65,12 @@ class Miner(Module):
     @endpoint
     def forwardPredictionSynapse(self, synapse: PredictionSynapse):
         pass
+    
+    @endpoint
+    def forwardCurrentPoolMetricSynapse(self, synapse: dict):
+        print('forwardCurrnetPoolMetricSynapse called')
+        response = CurrentPoolMetricResponse().json()
+        return response
 
 
 if __name__ == "__main__":
@@ -73,12 +80,12 @@ if __name__ == "__main__":
     from communex.module.server import ModuleServer
     import uvicorn
 
-    key = classic_load_key("your_key_file")
+    key = classic_load_key("your_key_here")
     miner = Miner()
     refill_rate = 1 / 400
     # Implementing custom limit
     bucket = TokenBucketLimiter(20, refill_rate)
-    server = ModuleServer(miner, key, limiter=bucket, subnets_whitelist=[41], use_testnet=True)
+    server = ModuleServer(miner, key, limiter=bucket, subnets_whitelist=[30], use_testnet=False)
     app = server.get_fastapi_app()
     # token0 = "0xaea46a60368a7bd060eec7df8cba43b7ef41ad85"
     # token1 = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
@@ -88,4 +95,4 @@ if __name__ == "__main__":
     # print(uniswap_fetcher_rs.fetch_pool_data_py(token0, token1, start_datetime, end_datetime, interval))
 
     # Only allow local connections
-    uvicorn.run(app, host="0.0.0.0", port=9962)
+    uvicorn.run(app, host="0.0.0.0", port=9900)
