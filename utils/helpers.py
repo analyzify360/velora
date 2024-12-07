559be02590add046e56cede0c81d9e81c2c78358
drunest <venus1004@proton.me>
2024-12-07 09:15:24 -0600

    feat: add new helpers

--- utils/helpers.py
+++ utils/helpers.py
@@ -22,3 +22,11 @@ def unsigned_hex_to_int(hex_str: str) -> int:
 def tick_to_sqrt_price(tick: int) -> float:
     """Convert a tick to the square root price"""
     return 1.0001 ** (tick / 2)
+
+def apply_abs_to_list(values: list[int]) -> list[int]:
+    """Apply the abs function to a list of values"""
+    return [abs(value) for value in values]
+
+def normalize_with_deciamls(value: int, token_decimals: int) -> float:
+    """Calculate the value with removing the decimals"""
+    return float(value) / 10**token_decimals
